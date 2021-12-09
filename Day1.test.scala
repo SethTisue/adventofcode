beae463cd1c7336c6eb41aaab3007975600abd26
Seth Tisue <seth@tisue.net>
2021-12-08 16:50:40 -0800

    day 1 part 2

--- Day1.test.scala
+++ Day1.test.scala
@@ -1,7 +1,20 @@
 // using scala 3.1.1-RC1
+// using option -source:future
 // using lib org.scalameta::munit::0.7.29
 
 class Day1 extends munit.FunSuite:
+
+  val sample = List(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+
+  // helpers
+
+  def intsFromFile(path: String): List[Int] =
+    io.Source.fromFile(path)
+      .getLines
+      .flatMap(_.toIntOption)
+      .toList
+
+  // Part 1
 
   def increases(xs: List[Int]): Int =
     xs.sliding(2).count {
@@ -9,12 +22,26 @@ class Day1 extends munit.FunSuite:
         cur > prev
     }
 
-  test("sample") {
-    val depths = List(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
-    assertEquals(increases(depths), 7)
+  test("part 1 sample") {
+    assertEquals(increases(sample), 7)
   }
 
-  test("real") {
-    val depths = io.Source.fromFile("day1.txt").getLines.flatMap(_.toIntOption).toList
-    assertEquals(increases(depths), 1713)
+  test("part 1 real") {
+    assertEquals(increases(intsFromFile("day1.txt")), 1713)
   }
+
+  // Part 2
+
+  def sliding3Increases(xs: List[Int]): Int =
+    increases(
+      xs.sliding(3).map(_.sum).toList)
+
+  test("part 2 sample") {
+    assertEquals(sliding3Increases(sample), 5)
+  }
+
+  test("part 2 real") {
+    assertEquals(sliding3Increases(intsFromFile("day1.txt")), 1734)
+  }
+
+end Day1
