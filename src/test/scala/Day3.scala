f8534f4b5e2f54c0e633bc472e3a4ab089448175
Seth Tisue <seth@tisue.net>
2022-12-03 10:20:04 -0600

    day 3 part 1

--- src/test/scala/Day3.scala
+++ src/test/scala/Day3.scala
@@ -7,30 +7,28 @@ class Day3 extends munit.FunSuite:
       .getLines
       .toList
 
-  def score(ss: List[String]): Int = -1
+  def scoreItem(c: Char): Int =
+    if c >= 'a' && c <= 'z' then
+      c -'a' + 1
+    else if c >= 'A' && c <= 'Z' then
+      c -'A' + 27
+    else
+      throw new IllegalArgumentException(c.toString)
 
-  // part 1
+  def score(knapsack: String): Int =
+    val (sack1, sack2) = knapsack.splitAt(knapsack.size / 2)
+    val commonItems = sack1.intersect(sack2).distinct
+    assert(commonItems.size == 1)
+    scoreItem(commonItems.head)
 
   // part 1 tests
 
   test("day 3 part 1 sample") {
-    assertEquals(0, score(getInput("day3-sample.txt")))
+    assertEquals(157, getInput("day3-sample.txt").map(score).sum)
   }
 
   test("day 3 part 1") {
-    assertEquals(0, score(getInput("day3.txt")))
-  }
-
-  // part 2
-
-  // part 2 tests
-
-  test("day 3 part 2 sample") {
-    assertEquals(0, score(getInput("day3-sample.txt")))
-  }
-
-  test("day 3 part 2") {
-    assertEquals(0, score(getInput("day3.txt")))
+    assertEquals(7997, getInput("day3.txt").map(score).sum)
   }
 
 end Day3
