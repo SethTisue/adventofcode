e9a1e12674c5839970ce987ab868c918f99f0d96
Seth Tisue <seth@tisue.net>
2022-12-03 10:27:10 -0600

    day 3 part 2

--- src/test/scala/Day3.scala
+++ src/test/scala/Day3.scala
@@ -15,7 +15,9 @@ class Day3 extends munit.FunSuite:
     else
       throw new IllegalArgumentException(c.toString)
 
-  def score(knapsack: String): Int =
+  // part 1
+
+  def scorePart1(knapsack: String): Int =
     val (sack1, sack2) = knapsack.splitAt(knapsack.size / 2)
     val commonItems = sack1.intersect(sack2).distinct
     assert(commonItems.size == 1)
@@ -24,11 +26,35 @@ class Day3 extends munit.FunSuite:
   // part 1 tests
 
   test("day 3 part 1 sample") {
-    assertEquals(157, getInput("day3-sample.txt").map(score).sum)
+    assertEquals(157, getInput("day3-sample.txt").map(scorePart1).sum)
   }
 
   test("day 3 part 1") {
-    assertEquals(7997, getInput("day3.txt").map(score).sum)
+    assertEquals(7997, getInput("day3.txt").map(scorePart1).sum)
+  }
+
+  // part 2
+
+  case class Group(elves: List[String])
+
+  def getInputPart2(name: String): List[Group] =
+    io.Source.fromResource(name)
+      .getLines
+      .grouped(3)
+      .map(knapsacks => Group(knapsacks.toList))
+      .toList
+
+  def scorePart2(group: Group): Int =
+    scoreItem(group.elves.reduceLeft(_.intersect(_)).head)
+
+  // part 2 tests
+
+  test("day 3 part 2 sample") {
+    assertEquals(70, getInputPart2("day3-sample.txt").map(scorePart2).sum)
+  }
+
+  test("day 3 part 2") {
+    assertEquals(2545, getInputPart2("day3.txt").map(scorePart2).sum)
   }
 
 end Day3
