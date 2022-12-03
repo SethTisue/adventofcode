8f32429b63aef05e00f9503cbc4c366c60827753
Seth Tisue <seth@tisue.net>
2022-12-03 10:35:32 -0600

    refactor

--- src/test/scala/Day3.scala
+++ src/test/scala/Day3.scala
@@ -2,26 +2,30 @@ class Day3 extends munit.FunSuite:
 
   // shared code
 
-  def getInput(name: String): List[String] =
+  type Item = Char
+  type Knapsack = String
+
+  def getInput(name: String): List[Knapsack] =
     io.Source.fromResource(name)
       .getLines
       .toList
 
-  def scoreItem(c: Char): Int =
-    if c >= 'a' && c <= 'z' then
-      c -'a' + 1
-    else if c >= 'A' && c <= 'Z' then
-      c -'A' + 27
+  def scoreItem(i: Item): Int =
+    if i >= 'a' && i <= 'z' then
+      i -'a' + 1
+    else if i >= 'A' && i <= 'Z' then
+      i -'A' + 27
     else
-      throw new IllegalArgumentException(c.toString)
+      throw new IllegalArgumentException(i.toString)
+
+  def scoreGroup(sacks: List[Knapsack]): Int =
+    scoreItem(sacks.reduceLeft(_.intersect(_)).head)
 
   // part 1
 
   def scorePart1(knapsack: String): Int =
     val (sack1, sack2) = knapsack.splitAt(knapsack.size / 2)
-    val commonItems = sack1.intersect(sack2).distinct
-    assert(commonItems.size == 1)
-    scoreItem(commonItems.head)
+    scoreGroup(List(sack1, sack2))
 
   // part 1 tests
 
@@ -35,26 +39,19 @@ class Day3 extends munit.FunSuite:
 
   // part 2
 
-  case class Group(elves: List[String])
-
-  def getInputPart2(name: String): List[Group] =
-    io.Source.fromResource(name)
-      .getLines
+  def getInputPart2(name: String): List[List[Knapsack]] =
+    getInput(name)
       .grouped(3)
-      .map(knapsacks => Group(knapsacks.toList))
       .toList
-
-  def scorePart2(group: Group): Int =
-    scoreItem(group.elves.reduceLeft(_.intersect(_)).head)
 
   // part 2 tests
 
   test("day 3 part 2 sample") {
-    assertEquals(70, getInputPart2("day3-sample.txt").map(scorePart2).sum)
+    assertEquals(70, getInputPart2("day3-sample.txt").map(scoreGroup).sum)
   }
 
   test("day 3 part 2") {
-    assertEquals(2545, getInputPart2("day3.txt").map(scorePart2).sum)
+    assertEquals(2545, getInputPart2("day3.txt").map(scoreGroup).sum)
   }
 
 end Day3
