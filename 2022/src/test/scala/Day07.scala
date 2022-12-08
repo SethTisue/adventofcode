fccb0d2fc850c8d732eb63dbcd77999db870346c
Seth Tisue <seth@tisue.net>
2022-12-07 20:46:08 -0800

    day 7: refactor

--- 2022/src/test/scala/Day07.scala
+++ 2022/src/test/scala/Day07.scala
@@ -1,6 +1,6 @@
 class Day07 extends munit.FunSuite:
 
-  // shared code
+  // I/O
 
   def getInput(name: String): List[Command] =
     io.Source.fromResource(name)
@@ -8,14 +8,26 @@ class Day07 extends munit.FunSuite:
       .map(Command.fromString)
       .toList
 
+  // data model & parsing: directory tree
+
   import collection.mutable.ListBuffer
 
   enum Entry:
     case Directory(name: String, entries: ListBuffer[Entry])
     case File(name: String, size: Long)
-
+  object Entry:
+    def fromString(s: String) = s match
+      case s"dir $name" => Entry.Directory(name, ListBuffer.empty)
+      case s"$size $name" => Entry.File(name, size.toLong)
   def root: Entry.Directory =
     Entry.Directory("/", ListBuffer())
+
+  def totalSize(dir: Entry.Directory): Long =
+    val files = dir.entries.collect{case f: Entry.File => f}
+    val dirs = dir.entries.collect{case d: Entry.Directory => d}
+    files.map(_.size).sum + dirs.map(totalSize).sum
+
+  // data model & parsing: commands
 
   enum Command:
     case Cd(dest: String)
@@ -27,11 +39,12 @@ class Day07 extends munit.FunSuite:
       case s"$$ cd $dest" => Cd(dest)
       case _ => Output(s)
 
+  // interpreter
+
   def runCommands(lines: List[Command], dirs: List[Entry.Directory]): Unit =
     lines match
       case Nil =>
       case line :: more =>
-        // println(s"@@@ $line")
         line match
           case Command.Cd("/") =>
             runCommands(more, List(dirs.last))
@@ -45,29 +58,10 @@ class Day07 extends munit.FunSuite:
           case Command.Ls =>
             val (outputLines, more2) = more.span(_.isInstanceOf[Command.Output])
             for Command.Output(s) <- outputLines.map(_.asInstanceOf[Command.Output])
-            do
-              // println(s"*** $s")
-              dirs.head.entries += (s match
-                case s"dir $name" => Entry.Directory(name, ListBuffer.empty)
-                case s"$size $name" => Entry.File(name, size.toLong)
-              )
-              // println(s"!!! ${dirs.head.entries.size}")
+            do dirs.head.entries += Entry.fromString(s)
             runCommands(more2, dirs)
           case _ =>
             throw new IllegalStateException(line.toString)
-
-  def totalSize(dir: Entry.Directory): Long =
-    val files = dir.entries.collect{case f: Entry.File => f}
-    val dirs = dir.entries.collect{case d: Entry.Directory => d}
-    files.map(_.size).sum + dirs.map(totalSize).sum
-
-  def dump(root: Entry, nestingLevel: Int = 0): Unit =
-    println((" " * nestingLevel) + root)
-    root match
-      case d: Entry.Directory => 
-        for e <- d.entries
-        do dump(e, nestingLevel + 2)
-      case _ =>
 
   // part 1 code
 
@@ -77,18 +71,14 @@ class Day07 extends munit.FunSuite:
       val size = totalSize(dir)
       if size <= 100000L then
         sum += size
-        // println(s"--- $size")
       dir.entries.collect{case d: Entry.Directory => d}.foreach(recurse)
     recurse(root)
-    // println(s"+++ $sum")
     sum
 
   // part 2 code
 
   def solve2(root: Entry.Directory): Long =
     val sizeNeeded = totalSize(root) - 40000000L
-    // println(s"*** sizeNeeded = $sizeNeeded")
-    // println(s"*** totalSize(root) = ${totalSize(root)}")
     def allSubdirs(root: Entry.Directory): Iterator[Entry.Directory] =
       Iterator(root) ++ root.entries.collect{case d: Entry.Directory => d}.iterator.flatMap(allSubdirs)
     allSubdirs(root).map(totalSize).filter(_ >= sizeNeeded).min
@@ -99,7 +89,6 @@ class Day07 extends munit.FunSuite:
     val lines = getInput("day07-sample.txt")
     val myRoot = root
     runCommands(lines, List(myRoot))
-    // dump(myRoot)
     assertEquals(95437L, solve1(myRoot))
   }
 
@@ -116,7 +105,6 @@ class Day07 extends munit.FunSuite:
     val lines = getInput("day07-sample.txt")
     val myRoot = root
     runCommands(lines, List(myRoot))
-    // dump(myRoot)
     assertEquals(24933642L, solve2(myRoot))
   }
 
@@ -124,7 +112,6 @@ class Day07 extends munit.FunSuite:
     val lines = getInput("day07.txt")
     val myRoot = root
     runCommands(lines, List(myRoot))
-    // dump(myRoot)
     assertEquals(4443914L, solve2(myRoot))
   }
 
