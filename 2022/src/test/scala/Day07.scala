a6c90e32c6eb975f2a93f8b4e77d86a2e7a85ad9
Seth Tisue <seth@tisue.net>
2022-12-07 20:21:01 -0800

    day 7 part 1 ugly solution

--- 2022/src/test/scala/Day07.scala
+++ 2022/src/test/scala/Day07.scala
@@ -22,24 +22,78 @@ class Day07 extends munit.FunSuite:
   object Command:
     def fromString(s: String) = s match
       case "$ ls" => Ls
-      case s"$$ cd $dest" => Cd (dest)
+      case s"$$ cd $dest" => Cd(dest)
       case _ => Output(s)
 
-  def solve(lines: List[Command], tree: Entry.Directory = root): Long =
+  def runCommands(lines: List[Command], dirs: List[Entry.Directory]): Unit =
     lines match
-      case Nil => 0
-      case line :: more => 0
+      case Nil =>
+      case line :: more =>
+        // println(s"@@@ $line")
+        line match
+          case Command.Cd("/") =>
+            runCommands(more, List(dirs.last))
+          case Command.Cd("..") =>
+            runCommands(more, dirs.tail)
+          case Command.Cd(dest) =>
+            val newCwd = dirs.head.entries.collectFirst{
+              case dir @ Entry.Directory(`dest`, _) => dir
+            }.get
+            runCommands(more, newCwd :: dirs)
+          case Command.Ls =>
+            val (outputLines, more2) = more.span(_.isInstanceOf[Command.Output])
+            for Command.Output(s) <- outputLines.map(_.asInstanceOf[Command.Output])
+            do
+              // println(s"*** $s")
+              dirs.head.entries += (s match
+                case s"dir $name" => Entry.Directory(name, ListBuffer.empty)
+                case s"$size $name" => Entry.File(name, size.toLong)
+              )
+              // println(s"!!! ${dirs.head.entries.size}")
+            runCommands(more2, dirs)
+          case _ =>
+            throw new IllegalStateException(line.toString)
+
+  def totalSize(dir: Entry.Directory): Long =
+    val files = dir.entries.collect{case f: Entry.File => f}
+    val dirs = dir.entries.collect{case d: Entry.Directory => d}
+    files.map(_.size).sum + dirs.map(totalSize).sum
+
+  def solve(root: Entry.Directory): Long =
+    var sum = 0L
+    def recurse(dir: Entry.Directory): Unit =
+      val size = totalSize(dir)
+      if size <= 100000L then
+        sum += size
+        // println(s"--- $size")
+      dir.entries.collect{case d: Entry.Directory => d}.foreach(recurse)
+    recurse(root)
+    // println(s"+++ $sum")
+    sum
+
+  def dump(root: Entry, nestingLevel: Int = 0): Unit =
+    println((" " * nestingLevel) + root)
+    root match
+      case d: Entry.Directory => 
+        for e <- d.entries
+        do dump(e, nestingLevel + 2)
+      case _ =>
 
   // part 1 tests
 
   test("day 7 part 1 sample") {
     val lines = getInput("day07-sample.txt")
-    assertEquals(95437L, solve(lines))
+    val myRoot = root
+    runCommands(lines, List(myRoot))
+    // dump(myRoot)
+    assertEquals(95437L, solve(myRoot))
   }
 
   test("day 7 part 1") {
     val lines = getInput("day07.txt")
-    assertEquals(0L, solve(lines))
+    val myRoot = root
+    runCommands(lines, List(myRoot))
+    assertEquals(1243729L, solve(myRoot))
   }
 
 end Day07
