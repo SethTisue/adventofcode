01a9a7ec4948c59098d85d429ffd5b12004bcb7a
Seth Tisue <seth@tisue.net>
2022-12-09 13:48:24 -0800

    day 9 part 1

--- 2022/src/test/scala/Day09.scala
+++ 2022/src/test/scala/Day09.scala
@@ -17,9 +17,34 @@ class Day09 extends munit.FunSuite:
        case s"$dir $dist" =>
          Command(Direction.fromChar(dir(0)), dist.toInt)
 
-  case class Location(x: Int, y: Int)
+  case class Location(x: Int, y: Int):
+    def move(dir: Direction): Location =
+      dir match
+        case Direction.Up    => copy(x, y - 1)
+        case Direction.Down  => copy(x, y + 1)
+        case Direction.Left  => copy(x - 1, y)
+        case Direction.Right => copy(x + 1, y)
+    def moveTail(tail: Location): Location =
+      val (dx, dy) =
+        (x - tail.x, y - tail.y) match
+          case ( 2, dy) => ( 1,  dy)
+          case (-2, dy) => (-1,  dy)
+          case (dx,  2) => (dx,   1)
+          case (dx, -2) => (dx,  -1)
+          case _        => ( 0,   0)
+      tail.copy(tail.x + dx, tail.y + dy)
 
-  def run(commands: Seq[Command]): Int = 13
+  def run(commands: Seq[Command]): Int =
+    var head = Location(0, 0)
+    var tail = head
+    var visited = List(tail)
+    for Command(direction, distance) <- commands
+        _ <- 0 until distance
+    do
+      head = head.move(direction)
+      tail = head.moveTail(tail)
+      visited ::= tail
+    visited.distinct.size
 
   // tests
 
@@ -34,6 +59,6 @@ class Day09 extends munit.FunSuite:
     }
 
   testDay9("part 1 sample", "day09-sample.txt", 13)
-  // testDay9("part 1",        "day09.txt",        1776)
+  testDay9("part 1",        "day09.txt",        5695)
 
 end Day09
