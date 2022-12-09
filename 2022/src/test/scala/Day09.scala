242e85a7b7cdcef7ba280f71a15332a22bf6a337
Seth Tisue <seth@tisue.net>
2022-12-09 15:35:15 -0800

    day 9: refactor

--- 2022/src/test/scala/Day09.scala
+++ 2022/src/test/scala/Day09.scala
@@ -1,5 +1,7 @@
 class Day09 extends munit.FunSuite:
 
+  // location and movement logic
+
   enum Direction(val dx: Int, val dy: Int):
     case U extends Direction( 0, -1)
     case D extends Direction( 0,  1)
@@ -7,6 +9,8 @@ class Day09 extends munit.FunSuite:
     case R extends Direction( 1,  0)
 
   case class Location(x: Int, y: Int)
+
+  val origin = Location(0, 0)
 
   def pullOne(first: Location, follower: Location): Location =
     val (dx, dy) = (first.x - follower.x, first.y - follower.y)
@@ -21,27 +25,36 @@ class Day09 extends munit.FunSuite:
       val pulled = pullOne(head, tail.head)
       pulled :: pullAll(pulled, tail.tail)
 
-  def run(moves: Iterator[Direction], tailSize: Int): Set[Location] =
-    var head = Location(0, 0)
-    var tail = List.fill(tailSize)(head)
-    var visited = Set(head)
-    for move <- moves
-    do
-      head = head.copy(head.x + move.dx, head.y + move.dy)
-      tail = pullAll(head, tail)
-      visited += tail.last
-    visited
+  // state management
+
+  case class State(head: Location, tail: List[Location], visited: Set[Location])
+
+  def initialState(tailSize: Int) =
+    State(origin, List.fill(tailSize)(origin), Set(origin))
+
+  def nextState(state: State, move: Direction): State =
+    val newHead = Location(state.head.x + move.dx, state.head.y + move.dy)
+    val newTail = pullAll(newHead, state.tail)
+    State(
+      head = newHead,
+      tail = newTail,
+      visited = state.visited + newTail.last)
+
+  def finalState(tailSize: Int, moves: Seq[Direction]): State =
+    moves.foldLeft(initialState(tailSize))(nextState)
 
   // tests
 
   def testDay9(name: String, file: String, tailSize: Int, answer: Int) =
     test(s"day 9 $name") {
-      val moves: Iterator[Direction] =
+      val moves: Seq[Direction] =
         io.Source.fromResource(file)
           .getLines
-          .flatMap{case s"$dir $dist" =>
-            List.fill(dist.toInt)(Direction.valueOf(dir))}
-      assertEquals(run(moves, tailSize).size, answer)
+          .flatMap{
+            case s"$dir $dist" =>
+              List.fill(dist.toInt)(Direction.valueOf(dir))
+          }.toSeq
+      assertEquals(finalState(tailSize, moves).visited.size, answer)
     }
 
   testDay9("part 1 sample",   "day09-sample1.txt", 1, 13  )
