3de9dc4a6c71451dd2d5d77c04eee14223e3874f
Seth Tisue <seth@tisue.net>
2022-12-09 15:01:53 -0800

    day 9 part 2

--- 2022/src/test/scala/Day09.scala
+++ 2022/src/test/scala/Day09.scala
@@ -24,41 +24,75 @@ class Day09 extends munit.FunSuite:
         case Direction.Down  => copy(x, y + 1)
         case Direction.Left  => copy(x - 1, y)
         case Direction.Right => copy(x + 1, y)
-    def moveTail(tail: Location): Location =
+    def pullOne(follower: Location): Location =
       val (dx, dy) =
-        (x - tail.x, y - tail.y) match
-          case ( 2, dy) => ( 1,  dy)
-          case (-2, dy) => (-1,  dy)
-          case (dx,  2) => (dx,   1)
-          case (dx, -2) => (dx,  -1)
+        (x - follower.x, y - follower.y) match
+          case ( 2, dy) => ( 1,  dy.sign)
+          case (-2, dy) => (-1,  dy.sign)
+          case (dx,  2) => (dx.sign,   1)
+          case (dx, -2) => (dx.sign,  -1)
           case _        => ( 0,   0)
-      tail.copy(tail.x + dx, tail.y + dy)
+      follower.copy(follower.x + dx, follower.y + dy)
+    def pullAll(tail: List[Location]): List[Location] =
+      if tail.isEmpty then Nil
+      else
+        val pulled = pullOne(tail.head)
+        pulled :: pulled.pullAll(tail.tail)
 
-  def run(commands: Seq[Command]): Int =
+  def run(commands: Seq[Command], tailSize: Int, debug: Boolean = false): Int =
     var head = Location(0, 0)
-    var tail = head
-    var visited = List(tail)
+    var tail = List.fill(tailSize)(head)
+    var visited = List(head)
     for Command(direction, distance) <- commands
         _ <- 0 until distance
     do
+      if debug then println(direction)
       head = head.move(direction)
-      tail = head.moveTail(tail)
-      visited ::= tail
+      tail = head.pullAll(tail)
+      visited ::= tail.last
+      if debug then
+        dumpBoard(head :: tail)
+        println()
+    if debug then
+      println("FINAL:")
+      dumpBoard(visited, showIndices = false)
     visited.distinct.size
+
+  // useful for debugging
+
+  def dumpBoard(rope: List[Location], showIndices: Boolean = true): Unit =
+    val minX = rope.map(_.x).min.min(0)
+    val maxX = rope.map(_.x).max.max(0)
+    val minY = rope.map(_.y).min.min(0)
+    val maxY = rope.map(_.y).max.max(0)
+    for y <- minY to maxY do
+      for x <- minX to maxX
+      do
+        val index = rope.indexOf(Location(x, y))
+        print(index match
+          case -1 => '.'
+          case _ if !showIndices => '#'
+          case 0 => 'H'
+          case _ => ('0' + index).toChar
+        )
+      println
 
   // tests
 
-  def testDay9(name: String, file: String, answer: Int) =
+  def testDay9(name: String, file: String, tailSize: Int, answer: Int) =
     test(s"day 9 $name") {
       val commands =
         io.Source.fromResource(file)
           .getLines
           .map(Command.fromString)
           .toList
-      assertEquals(run(commands), answer)
+      assertEquals(run(commands, tailSize), answer)
     }
 
-  testDay9("part 1 sample", "day09-sample.txt", 13)
-  testDay9("part 1",        "day09.txt",        5695)
+  testDay9("part 1 sample",   "day09-sample1.txt", 1, 13  )
+  testDay9("part 1",          "day09.txt",         1, 5695)
+  testDay9("part 2 sample 1", "day09-sample1.txt", 9, 1   )
+  testDay9("part 2 sample 2", "day09-sample2.txt", 9, 36  )
+  testDay9("part 2",          "day09.txt",         9, 2434)
 
 end Day09
