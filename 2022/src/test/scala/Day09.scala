4b28fcf67e8d33fdc6ff86b751a4189904d7b1ce
Seth Tisue <seth@tisue.net>
2022-12-09 15:11:32 -0800

    day 9: remove debugging output

--- 2022/src/test/scala/Day09.scala
+++ 2022/src/test/scala/Day09.scala
@@ -22,42 +22,16 @@ class Day09 extends munit.FunSuite:
         val pulled = pullOne(tail.head)
         pulled :: pulled.pullAll(tail.tail)
 
-  def run(moves: Iterator[Direction], tailSize: Int, debug: Boolean = false): Int =
+  def run(moves: Iterator[Direction], tailSize: Int): Int =
     var head = Location(0, 0)
     var tail = List.fill(tailSize)(head)
     var visited = List(head)
     for move <- moves
     do
-      if debug then println(move)
       head = head.copy(head.x + move.dx, head.y + move.dy)
       tail = head.pullAll(tail)
       visited ::= tail.last
-      if debug then
-        dumpBoard(head :: tail)
-        println()
-    if debug then
-      println("FINAL:")
-      dumpBoard(visited, showIndices = false)
     visited.distinct.size
-
-  // useful for debugging
-
-  def dumpBoard(rope: List[Location], showIndices: Boolean = true): Unit =
-    val minX = rope.map(_.x).min.min(0)
-    val maxX = rope.map(_.x).max.max(0)
-    val minY = rope.map(_.y).min.min(0)
-    val maxY = rope.map(_.y).max.max(0)
-    for y <- minY to maxY do
-      for x <- minX to maxX
-      do
-        val index = rope.indexOf(Location(x, y))
-        print(index match
-          case -1 => '.'
-          case _ if !showIndices => '#'
-          case 0 => 'H'
-          case _ => ('0' + index).toChar
-        )
-      println
 
   // tests
 
