57f57ee226256f3933bb47b7d07fc1325e20ac13
Seth Tisue <seth@tisue.net>
2022-12-10 20:19:41 -0800

    day 10 part 1 almost right

---
+++ 2022/src/test/scala/Day10.scala
@@ -0,0 +1,43 @@
+class Day10 extends munit.FunSuite:
+
+  enum Instruction:
+    case Noop
+    case AddX(amount: Int)
+
+  import Instruction.*
+
+  case class State(x: Int, cycle: Int)
+
+  val initialState = State(x = 1, cycle = 0)
+
+  def run(state: State, instruction: Instruction): State =
+    instruction match
+      case Noop =>
+        state.copy(cycle = state.cycle + 1)
+      case AddX(amount) =>
+        state.copy(x = state.x + amount, cycle = state.cycle + 1)
+
+  // tests
+
+  def testDay10(name: String, file: String, expected: Int) =
+    test(s"day 10 $name") {
+      val input: Iterator[Instruction] =
+        io.Source.fromResource(file)
+          .getLines
+          .flatMap{
+            case "noop" =>
+              List(Noop)
+            case s"addx $amount" =>
+              List(Noop, AddX(amount.toInt))
+          }
+      val states = input.to(LazyList).scanLeft(initialState)(run)
+      states.take(20).foreach(println)
+      import util.chaining.*
+      val result = (20 until states.size by 40).map(states.apply).tapEach(println).map(s => s.cycle * s.x).tapEach(println).sum
+      assertEquals(result, expected)
+    }
+
+  testDay10("part 1 sample", "day10-sample.txt", 13140)
+//  testDay10("part 1",        "day10.txt",            0)
+
+end Day10
