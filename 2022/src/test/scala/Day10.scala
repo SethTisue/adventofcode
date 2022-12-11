9fd37083eb77b6ef1b6d557a7ec0b5a24d97352c
Seth Tisue <seth@tisue.net>
2022-12-10 20:49:20 -0800

    day 10 part 1 refactor

--- 2022/src/test/scala/Day10.scala
+++ 2022/src/test/scala/Day10.scala
@@ -1,23 +1,26 @@
 class Day10 extends munit.FunSuite:
 
+  // part 1
+
+  def getInput(file: String): Iterator[Int] =
+    io.Source.fromResource(file).getLines
+      .flatMap {
+        case "noop"          => List(0)
+        case s"addx $amount" => List(0, amount.toInt)
+      }
+
   def signalStrengthSum(deltas: Iterator[Int]): Int =
     val xs = deltas.scanLeft(1)(_ + _).to(IndexedSeq)
     (20 to xs.size by 40)
       .map(n => xs(n - 1) * n)
       .sum
 
-  def testDay10(name: String, file: String, expected: Int) =
+  def testDay10Part1(name: String, file: String, expected: Int) =
     test(s"day 10 $name") {
-      val input: Iterator[Int] =
-        io.Source.fromResource(file).getLines
-          .flatMap {
-            case "noop"          => List(0)
-            case s"addx $amount" => List(0, amount.toInt)
-          }
-      assertEquals(signalStrengthSum(input), expected)
+      assertEquals(signalStrengthSum(getInput(file)), expected)
     }
 
-  testDay10("part 1 sample", "day10-sample.txt", 13140)
-  testDay10("part 1",        "day10.txt",        14320)
+  testDay10Part1("part 1 sample", "day10-sample.txt", 13140)
+  testDay10Part1("part 1",        "day10.txt",        14320)
 
 end Day10
