bad04612cc90a82d6c2781f1409c054dd096dcee
Seth Tisue <seth@tisue.net>
2022-12-11 08:52:23 -0800

    day 11 part 2 but it's wrong

--- 2022/src/test/scala/Day11.scala
+++ 2022/src/test/scala/Day11.scala
@@ -33,8 +33,9 @@ class Day11 extends munit.FunSuite:
       .split("\n\n").iterator
       .map(section => Monkey.fromString(section.split('\n')))
 
-  def runMonkeys(monkeys: collection.mutable.IndexedSeq[Monkey]): Int =
-    for round <- 1 to 20 do
+  def runMonkeys(monkeys: collection.mutable.IndexedSeq[Monkey], divisor: Int, rounds: Int): Long =
+    for round <- 1 to rounds do
+      println(round)
       for (m, i) <- monkeys.zipWithIndex do
         // println(s"Monkey $i:")
         for item <- m.items do
@@ -42,8 +43,8 @@ class Day11 extends munit.FunSuite:
           // println(s"  Monkey inspects an item with a worry level of $item")
           val newWorryLevel = m.operation(item)
           // println(s"    Worry level becomes $newWorryLevel.")
-          val reducedWorryLevel = newWorryLevel / 3
-          // println(s"    Monkey gets bored with item. Worry level is divided by 3 to $reducedWorryLevel")
+          val reducedWorryLevel = newWorryLevel / divisor
+          // println(s"    Monkey gets bored with item. Worry level is divided by $divisor to $reducedWorryLevel")
           if reducedWorryLevel % m.divisibleBy == 0 then
             monkeys(m.trueMonkey).items :+= reducedWorryLevel
             // println(s"    Current worry level is divisible by ${m.divisibleBy}.")
@@ -53,21 +54,32 @@ class Day11 extends munit.FunSuite:
             // println(s"    Current worry level is not divisible by ${m.divisibleBy}.")
             // println(s"    Item with worry level $reducedWorryLevel is thrown to monkey ${m.falseMonkey}.")
         m.items = Nil
-      println(s"After round $round, the monkeys are holding items with these worry levels:")
-      for (m, i) <- monkeys.zipWithIndex do
-        println(s"""Monkey $i: ${m.items.mkString(", ")}""")
-      println()
-    monkeys.map(_.inspected).sorted.takeRight(2).product
+      // println(s"After round $round, the monkeys are holding items with these worry levels:")
+      // for (m, i) <- monkeys.zipWithIndex do
+        // println(s"""Monkey $i: ${m.items.mkString(", ")}""")
+      // println()
+    monkeys.map(_.inspected).sorted.takeRight(2).map(_.toLong).product
 
   // part 1 tests
 
-  def testDay11Part1(name: String, file: String, expected: Int) =
+  def testDay11Part1(name: String, file: String, expected: Long) =
     test(s"day 11 $name") {
       val input = getInput(file).to(collection.mutable.IndexedSeq)
-      assertEquals(runMonkeys(input), expected)
+      assertEquals(runMonkeys(input, divisor = 3, rounds = 20), expected)
     }
 
   testDay11Part1("part 1 sample", "day11-sample.txt", 10605)
   testDay11Part1("part 1",        "day11.txt",        78960)
+
+  // part 2 tests
+
+  def testDay11Part2(name: String, file: String, expected: Long) =
+    test(s"day 11 $name") {
+      val input = getInput(file).to(collection.mutable.IndexedSeq)
+      assertEquals(runMonkeys(input, divisor = 1, rounds = 10000), expected)
+    }
+
+  testDay11Part2("part 2 sample", "day11-sample.txt", 2713310158L)
+  testDay11Part2("part 2",        "day11.txt",        0)
 
 end Day11
