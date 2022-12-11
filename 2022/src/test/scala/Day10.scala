a33c069c8a45c7ce919134cd5b0bb4c85f9f57bc
Seth Tisue <seth@tisue.net>
2022-12-10 20:42:09 -0800

    day 10 part 1 refactor

--- 2022/src/test/scala/Day10.scala
+++ 2022/src/test/scala/Day10.scala
@@ -1,31 +1,20 @@
 class Day10 extends munit.FunSuite:
 
-  case class State(x: Int)
-
-  val initialState = State(x = 1)
-
-  def run(state: State, delta: Int): State =
-    state.copy(x = state.x + delta)
-
-  // tests
+  def signalStrengthSum(deltas: Iterator[Int]): Int =
+    val xs = deltas.scanLeft(1)(_ + _).to(IndexedSeq)
+    (20 to xs.size by 40)
+      .map(n => xs(n - 1) * n)
+      .sum
 
   def testDay10(name: String, file: String, expected: Int) =
     test(s"day 10 $name") {
       val input: Iterator[Int] =
-        io.Source.fromResource(file)
-          .getLines
-          .flatMap{
-            case "noop" =>
-              List(0)
-            case s"addx $amount" =>
-              List(0, amount.toInt)
+        io.Source.fromResource(file).getLines
+          .flatMap {
+            case "noop"          => List(0)
+            case s"addx $amount" => List(0, amount.toInt)
           }
-      val states = input.to(LazyList).scanLeft(initialState)(run)
-      val result =
-        (20 until states.size by 40)
-          .map(n => states(n - 1).x * n)
-          .sum
-      assertEquals(result, expected)
+      assertEquals(signalStrengthSum(input), expected)
     }
 
   testDay10("part 1 sample", "day10-sample.txt", 13140)
