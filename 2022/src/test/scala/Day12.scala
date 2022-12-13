cceaedd18b3726110492357e2b30bf0972a5cd90
Seth Tisue <seth@tisue.net>
2022-12-12 17:21:41 -0800

    day 12 part 2

--- 2022/src/test/scala/Day12.scala
+++ 2022/src/test/scala/Day12.scala
@@ -1,3 +1,6 @@
+// This code is really awful but I don't feel like cleaning it up.
+// I just wanted to solve this as fast as possible and move on.
+
 class Day12 extends munit.FunSuite:
 
   def getInput(file: String): Vector[Vector[Char]] =
@@ -7,27 +10,33 @@ class Day12 extends munit.FunSuite:
       .to(Vector)
       .transpose
 
-  // this is some craptastic code but I want to wait to clean it up
-  // until I've seen what part 2 needs
-
-  def solve(grid: Vector[Vector[Char]], initialY: Int, maxX: Int, maxY: Int): Int =
+  def solve(grid: Vector[Vector[Char]], initialX: Int, initialY: Int, maxX: Int, maxY: Int, uphill: Boolean): Int =
     val distances: Array[Array[Int]] =
       Array.fill(maxX)(Array.fill(maxY)(-1))
-    var queue = Vector((0, initialY, 0))  // reaching (0,initialY) takes 0 steps
-    require(grid(0)(initialY) == 'S')
-    distances(0)(initialY) = 0
+    var queue = Vector((initialX, initialY, 0))
+    distances(initialX)(initialY) = 0
+    val endDest = if uphill then 'E' else 'a'
     while queue.nonEmpty do
       val here = (queue.head._1, queue.head._2)
       val steps = (queue.head._3)
       distances(here._1)(here._2) = steps
       queue = queue.tail
+      def reachable(src: Char, dest: Char) =
+        val effectiveSrc = if src == 'S' then 'a' else if src == 'E' then 'z' else src
+        val effectiveDest = if dest == 'E' then 'z' else if dest == 'S' then 'a' else dest
+        if uphill then effectiveDest - effectiveSrc <= 1
+        else effectiveDest - effectiveSrc >= -1
       def next(dx: Int)(dy: Int): Boolean =
-        if here._1 + dx >= 0 && here._1 + dx < maxX && here._2 + dy >= 0 && here._2 + dy < maxY then
-          if (grid(here._1)(here._2) == 'z' || grid(here._1)(here._2) == 'y')&& grid(here._1 + dx)(here._2 + dy) == 'E' then
+        val valueHere = grid(here._1)(here._2)
+        val newX = here._1 + dx
+        val newY = here._2 + dy
+        if newX >= 0 && newX < maxX && newY >= 0 && newY < maxY then
+          if reachable(src = valueHere, dest = grid(newX)(newY)) && grid(newX)(newY) == endDest then
             return true
-          if distances(here._1 + dx)(here._2 + dy) == -1 then
-            if (grid(here._1)(here._2) == 'S' || grid(here._1 + dx)(here._2 + dy) <= grid(here._1)(here._2) + 1) && !queue.exists(entry => entry._1 == here._1 + dx && entry._2 == here._2 + dy) then
-              queue :+= (here._1 + dx, here._2 + dy, steps + 1)
+          if distances(newX)(newY) == -1 then
+            val dest = grid(newX)(newY)
+            if reachable(src = valueHere, dest = grid(newX)(newY)) && !queue.exists(entry => entry._1 == newX && entry._2 == newY) then
+              queue :+= (newX, newY, steps + 1)
         false
       if next(1)(0) then return steps + 1
       if next(0)(1) then return steps + 1
@@ -35,13 +44,23 @@ class Day12 extends munit.FunSuite:
       if next(-1)(0) then return steps + 1
     throw new IllegalStateException
 
-  def testDay12(name: String, file: String, expected: Int) =
+  def testDay12(name: String, file: String, uphill: Boolean, expected: Int) =
     test(s"day 12 $name") {
       val grid = getInput(file)
-      assertEquals(solve(grid, maxX = grid.size, maxY = grid.head.size, initialY = grid.head.indexOf('S')), expected)
+      val start = if uphill then 'S' else 'E'
+      val initialX = grid.indexWhere(_.contains(start))
+      val initialY = grid(initialX).indexOf(start)
+      assertEquals(
+        solve(grid, maxX = grid.size, maxY = grid.head.size,
+          initialX = initialX, initialY = initialY,
+          uphill = uphill),
+        expected)
     }
 
-  testDay12("part 1 sample", "day12-sample.txt",  31)
-  testDay12("part 1",        "day12.txt",        481)
+  testDay12("part 1 sample", "day12-sample.txt",  true,   31)
+  testDay12("part 1",        "day12.txt",         true,  481)
+
+  testDay12("part 2 sample", "day12-sample.txt",  false,  29)
+  testDay12("part 2",        "day12.txt",         false, 480)
 
 end Day12
