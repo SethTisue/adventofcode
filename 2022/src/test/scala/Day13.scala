e66d877d7d5bbf70a073b1d2195ccd9ee1fb37d3
Seth Tisue <seth@tisue.net>
2022-12-13 19:46:25 -0800

    day 13: pure-functional parsing code

--- 2022/src/test/scala/Day13.scala
+++ 2022/src/test/scala/Day13.scala
@@ -4,37 +4,39 @@ class Day13 extends munit.FunSuite:
     case Number(n: Int)
     case Nested(ps: List[Packet])
   object Packet:
+    def toString(p: Packet): String = p match
+      case Packet.Number(n) => n.toString
+      case Packet.Nested(ps) => ps.map(Packet.toString).mkString("[",",","]")
     def fromString(s: String): Packet =
       val (result, remaining) = parse(s)
       require(remaining.isEmpty, remaining)
-      println(result)
+      require(Packet.toString(result) == s)
       result
     def parse(s: String): (Packet, String) =
       val (digits, more) = s.span(_.isDigit)
       if digits.nonEmpty then
         (Packet.Number(digits.toInt), more)
       else
-        require(s.head == '[')
-        var remaining = s.tail
-        var buf = Vector[Packet]()
-        while remaining.head != ']' do
-          val (next, rem2) = parse(remaining)
-          buf :+= next
-          if rem2.head == ',' then
-            remaining = rem2.tail
+        require(s.head == '[', s.head.toString)
+        def recurse(s2: String): (List[Packet], String) =
+          if s2.head == ']'
+          then (Nil, s2.tail)
           else
-            require(rem2.head == ']')
-            remaining = rem2
-        end while
-        (Packet.Nested(buf.toList), remaining.tail)
+            val (next, s3) = parse(s2)
+            val s3b = if s3.head == ',' then s3.tail else s3
+            val (rest, s4) = recurse(s3b)
+            (next :: rest, s4)
+        val (xs, foo) = recurse(s.tail)
+        (Packet.Nested(xs), foo)
 
-  def getInput(file: String): Iterator[String] =
+  def getInput(file: String): Iterator[Packet] =
     io.Source.fromResource(file)
       .getLines
+      .filter(_.nonEmpty)
+      .map(Packet.fromString)
 
-  def solve(input: Iterator[String]): Int =
-    def one = Packet.fromString(input.next()).asInstanceOf[Packet.Nested].ps.size
-    List(one, one, { input.next(); one }, one).sum
+  def solve(input: Iterator[Packet]): Int =
+    input.size
 
   def testDay13(name: String, file: String, expected: Int) =
     test(s"day 13 $name") {
