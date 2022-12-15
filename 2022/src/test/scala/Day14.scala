c1e4890dc1be66932e8596429027680cfde933ff
Seth Tisue <seth@tisue.net>
2022-12-15 14:36:21 -0800

    day 14 refactor

--- 2022/src/test/scala/Day14.scala
+++ 2022/src/test/scala/Day14.scala
@@ -1,5 +1,7 @@
 class Day14 extends munit.FunSuite:
 
+  // data
+
   case class Point(x: Int, y: Int)
 
   type Path = List[Point]
@@ -9,45 +11,51 @@ class Day14 extends munit.FunSuite:
 
   type Grid = Array[Array[Square]]
 
-  def newGrid(): Grid =
-    Array.fill(1000)(Array.fill(200)(Square.Empty))
+  // logic
 
-  def testDay14(name: String, file: String, expected: Int) =
-    def getInput(file: String): Iterator[Path] =
-      io.Source.fromResource(file)
-        .getLines
-        .map(pathFromString)
+  def dropSand(grid: Grid, loc: Point): Boolean = // true if the sand stopped
+    val newY = loc.y + 1
+    newY < 200 && {
+      val candidates = List(loc.x, loc.x - 1, loc.x + 1)
+      candidates.find(newX => grid(newX)(newY) == Square.Empty) match
+        case Some(newX) =>
+          dropSand(grid, Point(newX, newY))
+        case None =>
+          grid(loc.x)(loc.y) = Square.Sand
+          true
+    }
+
+  // reading the input
+
+  def getInput(file: String): Grid =
     def pathFromString(s: String): Path =
       s.split(" -> ")
         .map{case s"$x,$y" => Point(x.toInt, y.toInt)}
         .toList
     def pointsFromSegment(p1: Point, p2: Point): List[Point] =
       if p1.x == p2.x then
-        (p1.y to p2.y by (p2.y - p1.y).sign).map(y => Point(p1.x, y)).toList
+        (p1.y to p2.y by (p2.y - p1.y).sign)
+          .map(y => Point(p1.x, y)).toList
       else
-        (p1.x to p2.x by (p2.x - p1.x).sign).map(x => Point(x, p1.y)).toList
+        (p1.x to p2.x by (p2.x - p1.x).sign)
+          .map(x => Point(x, p1.y)).toList
     def gridFromPaths(paths: Iterator[Path]): Grid =
-      val grid = newGrid()
+      val grid = Array.fill(1000)(Array.fill(200)(Square.Empty))
       for path <- paths
           List(end1, end2) <- path.sliding(2)
           point <- pointsFromSegment(end1, end2)
-      do
-        grid(point.x)(point.y) = Square.Rock
+      do grid(point.x)(point.y) = Square.Rock
       grid
+    gridFromPaths(
+      io.Source.fromResource(file)
+        .getLines
+       .map(pathFromString))
 
-    def dropSand(grid: Grid, loc: Point): Boolean = // true if the sand stopped
-      val newY = loc.y + 1
-      newY < 200 && {
-        List(loc.x, loc.x - 1, loc.x + 1).find(newX => grid(newX)(newY) == Square.Empty) match
-          case Some(newX) =>
-            dropSand(grid, Point(newX, newY))
-          case None =>
-            grid(loc.x)(loc.y) = Square.Sand
-            true
-      }
+  // part 1 tests
 
+  def testDay14(name: String, file: String, expected: Int) =
     test(s"day 14 $name") {
-      val grid = gridFromPaths(getInput(file))
+      val grid = getInput(file)
       assertEquals(
         Iterator.continually(dropSand(grid, Point(500, 0))).takeWhile(_ == true).size,
         expected)
