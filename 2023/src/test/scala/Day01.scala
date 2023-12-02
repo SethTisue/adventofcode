6417b531e5228d59843eacf92436f43a66d19b20
Seth Tisue <seth@tisue.net>
2023-12-01 22:02:58 -0800

    minor code improvement

--- 2023/src/test/scala/Day01.scala
+++ 2023/src/test/scala/Day01.scala
@@ -10,12 +10,12 @@ class Day01 extends munit.FunSuite:
     val digits = line.filter(_.isDigit)
     s"${digits.head}${digits.last}".toInt
 
-  test("day 1 part 1 sample"):
+  test("part 1 sample"):
     assertEquals(142,
       getInput("day01-sample1.txt")
         .map(calibrationValuePart1).sum)
 
-  test("day 1 part 1"):
+  test("part 1"):
     assertEquals(56042,
       getInput("day01.txt")
         .map(calibrationValuePart1).sum)
@@ -38,12 +38,12 @@ class Day01 extends munit.FunSuite:
         else Some(index)
     digits.head * 10 + digits.last
 
-  test("day 1 part 2 sample"):
+  test("part 2 sample"):
     assertEquals(281,
       getInput("day01-sample2.txt")
         .map(calibrationValuePart2).sum)
 
-  test("day 1 part 2"):
+  test("part 2"):
     assertEquals(55358,
       getInput("day01.txt")
         .map(calibrationValuePart2).sum)
