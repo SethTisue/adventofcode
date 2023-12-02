adbb9cb2aa4e0336703619991563641de900cc84
Seth Tisue <seth@tisue.net>
2023-12-01 21:53:42 -0800

    fewer braces

--- 2023/src/test/scala/Day01.scala
+++ 2023/src/test/scala/Day01.scala
@@ -10,17 +10,15 @@ class Day01 extends munit.FunSuite:
     val digits = line.filter(_.isDigit)
     s"${digits.head}${digits.last}".toInt
 
-  test("day 1 part 1 sample") {
+  test("day 1 part 1 sample"):
     assertEquals(142,
       getInput("day01-sample1.txt")
         .map(calibrationValuePart1).sum)
-  }
 
-  test("day 1 part 1") {
+  test("day 1 part 1"):
     assertEquals(56042,
       getInput("day01.txt")
         .map(calibrationValuePart1).sum)
-  }
 
   // part 2
 
@@ -40,16 +38,14 @@ class Day01 extends munit.FunSuite:
         else Some(index)
     digits.head * 10 + digits.last
 
-  test("day 1 part 2 sample") {
+  test("day 1 part 2 sample"):
     assertEquals(281,
       getInput("day01-sample2.txt")
         .map(calibrationValuePart2).sum)
-  }
 
-  test("day 1 part 2") {
+  test("day 1 part 2"):
     assertEquals(55358,
       getInput("day01.txt")
         .map(calibrationValuePart2).sum)
-  }
 
 end Day01
