8e1d7c229ed4eaa4649618614f1ee44add60f5be
Seth Tisue <seth@tisue.net>
2023-12-03 06:52:58 -0800

    day 3 skeleton

---
+++ 2023/src/test/scala/Day03.scala
@@ -0,0 +1,44 @@
+class Day03 extends munit.FunSuite:
+
+  // data model
+
+  enum Entry:
+    case Empty
+    case Symbol
+    case Digit(n: Int)
+  object Entry:
+    def fromChar(c: Char): Entry =
+      if c == '.' then Empty
+      else if c.isDigit then Digit(c - '0')
+      else Symbol
+
+  type Schematic = Vector[Vector[Entry]]
+
+  // reading and parsing
+
+  def getInput(name: String): Schematic =
+    io.Source.fromResource(name)
+      .getLines.toVector.map: line =>
+        line.toVector.map(Entry.fromChar)
+
+  // part 1
+
+  def part1(name: String): Int = ???
+
+  test("part 1 sample"):
+    assertEquals(4361, part1("day03-sample.txt"))
+  test("part 1"):
+    assertEquals(0, part1("day03.txt"))
+
+/*
+  // part 2
+
+  def part2(name: String): Int = ???
+
+  test("part 2 sample"):
+    assertEquals(0, part2("day02-sample.txt"))
+  test("part 2"):
+    assertEquals(0, part2("day02.txt"))
+*/
+
+end Day03
