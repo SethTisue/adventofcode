ca94465ec7d5a9d23e512a9bc95a5cf921222efa
Seth Tisue <seth@tisue.net>
2023-12-05 21:36:16 -0800

    day 6 part 2

--- 2023/src/test/scala/Day06.scala
+++ 2023/src/test/scala/Day06.scala
@@ -2,29 +2,27 @@ class Day06 extends munit.FunSuite:
 
   /// data model
 
-  case class Race(time: Int, record: Int)
-
-  /// reading and parsing
-
-  def getInput(name: String): Vector[Race] =
-    val lines = io.Source.fromResource(name).getLines
-    val times =
-      val s"Time: $rest" = lines.next(): @unchecked
-      rest.trim.split("\\s+").map(_.toInt).toVector
-    val records =
-      val s"Distance: $rest" = lines.next(): @unchecked
-      rest.trim.split("\\s+").map(_.toInt).toVector
-    for (time, record) <- times.zip(records)
-    yield Race(time, record)
+  case class Race(time: Long, record: Long):
+    def ways: Long =
+      (1L until time)
+       .map: holdTime =>
+         holdTime * (time - holdTime)
+       .count:
+         _ > record
 
   /// part 1
 
+  def getInput(name: String): (Seq[Int], Seq[Int]) =
+    val lines = io.Source.fromResource(name).getLines
+    def parse(line: String): Seq[Int] =
+      val s"$_: $rest" = line: @unchecked
+      rest.trim.split("\\s+").map(_.toInt).toVector
+    (parse(lines.next()), parse(lines.next()))
+
   def part1(name: String): Long =
-    getInput(name)
-      .map: race =>
-        (1 until race.time)
-          .map(holdTime => holdTime * (race.time - holdTime))
-          .count(_ > race.record)
+    val (times, records) = getInput(name)
+    times.zip(records).map(Race(_, _))
+      .map(_.ways)
       .product
 
   test("part 1 sample"):
@@ -32,17 +30,16 @@ class Day06 extends munit.FunSuite:
   test("part 1"):
     assertEquals(part1("day06.txt"), 781200L)
 
-/*
   /// part 2
 
   def part2(name: String): Long =
-    val input = getInput(name)
-    0L
+    val (times, records) = getInput(name)
+    Race(times.mkString.toLong, records.mkString.toLong)
+      .ways
 
   test("part 2 sample"):
-    assertEquals(part2("day06-sample.txt"), 0L)
+    assertEquals(part2("day06-sample.txt"), 71503L)
   test("part 2"):
-    assertEquals(part2("day06.txt"), 0L)
- */
+    assertEquals(part2("day06.txt"), 49240091L)
 
 end Day06
