276f65b087cff1366003b925112eb45bb54ceb7d
Seth Tisue <seth@tisue.net>
2023-12-07 08:51:11 -0800

    day 7: refactor for part 2

--- 2023/src/test/scala/Day07.scala
+++ 2023/src/test/scala/Day07.scala
@@ -8,8 +8,6 @@ class Day07 extends munit.FunSuite:
   def cardCounts(hand: Hand): Seq[Int] =
     hand.groupBy(identity).map(_._2.size).toSeq.sorted.reverse
 
-  val cards = "23456789TJQKA"
-
   val types = Seq(
     Seq(1, 1, 1, 1, 1), // high card
     Seq(2, 1, 1, 1),    // one pair
@@ -20,31 +18,16 @@ class Day07 extends munit.FunSuite:
     Seq(5),             // five of a kind
   )
 
-  given Ordering[Hand] with
-    def tieBreaker(h1: Hand, h2: Hand): Int =
-      if h1.isEmpty
-      then
-        require(h2.isEmpty)
-        0
-      else
-        val result =
-          summon[Ordering[Int]].compare(
-            cards.indexOf(h1.head),
-            cards.indexOf(h2.head))
-        if result != 0
-        then result
-        else tieBreaker(h1.tail, h2.tail)
-    def compare(h1: Hand, h2: Hand): Int =
-      val result =
-        summon[Ordering[Int]]
-          .compare(
-            types.indexOf(cardCounts(h1)).ensuring(_ != -1, s"$h1 ${cardCounts(h1)}"),
-            types.indexOf(cardCounts(h2)).ensuring(_ != -1))
-      if result != 0
-      then result
-      else tieBreaker(h1, h2)
+  def winnings(input: Vector[(Hand, Bid)])(using Ordering[Hand]): Long =
+    input
+      .sortBy(_._1)
+      .map(_._2)
+      .zipWithIndex
+      .map: (bid, index) =>
+        bid * (index + 1)
+      .sum
 
-  /// part 1
+  /// reading & parsing
 
   def getInput(name: String): Vector[(Hand, Bid)] =
     io.Source.fromResource(name)
@@ -54,20 +37,58 @@ class Day07 extends munit.FunSuite:
           (hand.toSeq, bid.toInt)
       .toVector
 
+  /// part 1
+
+  def part1Ordering(cards: String) =
+    new Ordering[Hand]:
+      def tieBreaker(h1: Hand, h2: Hand): Int =
+        if h1.isEmpty
+        then
+          require(h2.isEmpty)
+          0
+        else
+          val result =
+            summon[Ordering[Int]].compare(
+              cards.indexOf(h1.head),
+              cards.indexOf(h2.head))
+          if result != 0
+          then result
+          else tieBreaker(h1.tail, h2.tail)
+      def compare(h1: Hand, h2: Hand): Int =
+        val result =
+          summon[Ordering[Int]]
+            .compare(
+              types.indexOf(cardCounts(h1)),
+              types.indexOf(cardCounts(h2)))
+        if result != 0
+        then result
+        else tieBreaker(h1, h2)
+
   def part1(name: String): Long =
-    import util.chaining.*
-    getInput(name)
-      .sortBy(_._1)
-      .tapEach(println)
-      .map(_._2)
-      .zipWithIndex
-      .map: (bid, index) =>
-        bid * (index + 1)
-      .sum
+    given Ordering[Hand] = part1Ordering("23456789TJQKA")
+    winnings(getInput(name))
 
   test("part 1 sample"):
     assertEquals(part1("day07-sample.txt"), 6440L)
   test("part 1"):
-    assertEquals(part1("day07.txt"), 0L)
+    assertEquals(part1("day07.txt"), 248569531L)
+
+  /// part 2
+
+  object part2Ordering extends Ordering[Hand]:
+    val delegate = part1Ordering("J23456789TQKA")
+    def compare(h1: Hand, h2: Hand): Int =
+      delegate.compare(h1, h2)
+
+  def part2(name: String): Long =
+    given Ordering[Hand] = part2Ordering
+    winnings(getInput(name))
+
+  test("part 2 sample"):
+    assertEquals(part2("day07-sample.txt"), 5905L)
+/*
+  test("part 2"):
+    assertEquals(part2("day07.txt"), 0)
+ */
 
 end Day07
