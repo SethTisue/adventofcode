7d6126a8f7b1026c25d2f995434714178b2bffd9
Seth Tisue <seth@tisue.net>
2023-12-07 21:41:09 -0800

    day 8 part 2

--- 2023/src/test/scala/Day08.scala
+++ 2023/src/test/scala/Day08.scala
@@ -9,16 +9,13 @@ class Day08 extends munit.FunSuite:
   case class Node(left: String, right: String)
   type Network = Map[String, Node]
 
-  def navigate(moves: Seq[Move], network: Network): LazyList[String] =
+  def navigate(start: String, moves: Seq[Move], network: Network): LazyList[String] =
     def recurse(name: String, moves: LazyList[Move]): LazyList[String] =
-      if name == "ZZZ"
-      then LazyList()
-      else
-        val next = moves.head match
-          case Move.Left => network(name).left
-          case Move.Right => network(name).right
-        next #:: recurse(next, moves.tail)
-    recurse("AAA", LazyList.continually(moves).flatten)
+      val next = moves.head match
+        case Move.Left => network(name).left
+        case Move.Right => network(name).right
+      next #:: recurse(next, moves.tail)
+    recurse(start, LazyList.continually(moves).flatten)
 
   /// reading & parsing
 
@@ -39,23 +36,26 @@ class Day08 extends munit.FunSuite:
 
   def part1(name: String): Int =
     val (moves, network) = getInput(name)
-    navigate(moves, network).size
+    navigate("AAA", moves, network).takeWhile(_ != "ZZZ").size + 1
 
   test("part 1 sample"):
     assertEquals(part1("day08-sample.txt"), 6)
   test("part 1"):
     assertEquals(part1("day08.txt"), 14681)
 
-/*
   /// part 2
 
   def part2(name: String): Int =
-    0
+    val (moves, network) = getInput(name)
+    @tailrec def recurse(routes: Seq[LazyList[String]], steps: Int): Int =
+      if routes.map(_.head).forall(_.endsWith("Z"))
+      then steps
+      else recurse(routes.map(_.tail), steps + 1)
+    recurse(network.keys.filter(_.endsWith("A")).toSeq.map(navigate(_, moves, network)), 1)
 
   test("part 2 sample"):
-    assertEquals(part2("day08-sample.txt"), 0)
+    assertEquals(part2("day08-sample2.txt"), 6)
   test("part 2"):
-    assertEquals(part2("day08.txt"), 0)
- */
+    assertEquals(part2("day08.txt"), -1)
 
 end Day08
