027cdc61c76156547fc6751ab9ef9ca683e09d03
Seth Tisue <seth@tisue.net>
2023-12-12 09:59:04 -0800

    day 12 is fast now

--- 2023/src/test/scala/Day12.scala
+++ 2023/src/test/scala/Day12.scala
@@ -2,28 +2,30 @@ class Day12 extends munit.FunSuite:
 
   /// core logic
 
-  def matches(states: Vector[Char], counts: Vector[Int], seen: Int = 0): Int =
-    if states.isEmpty
-    then
-      if (seen == 0 && counts.isEmpty) || counts == Vector(seen)
-      then 1
-      else 0
-    else states.head match
-      case '.' =>
-        if counts.headOption.contains(seen)
-        then matches(states.tail, counts.tail)
-        else if seen == 0
-        then matches(states.tail, counts)
-        else 0
-      case '#' =>
-        if counts.headOption.contains(seen)
-        then 0
-        else matches(states.tail, counts, seen + 1)
-      case '?' =>
-        matches('#' +: states.tail, counts, seen) +
-          matches('.' +: states.tail, counts, seen)
-      case c =>
-        require(false, s"invalid: $c"); ???
+  lazy val matches: (Vector[Char], Vector[Int], Int) => Long =
+    Memo.memoize:
+      (states: Vector[Char], counts: Vector[Int], seen: Int) =>
+        if states.isEmpty
+        then
+          if (seen == 0 && counts.isEmpty) || counts == Vector(seen)
+          then 1L
+          else 0L
+        else states.head match
+          case '.' =>
+            if counts.headOption.contains(seen)
+            then matches(states.tail, counts.tail, 0)
+            else if seen == 0L
+            then matches(states.tail, counts, 0)
+            else 0L
+          case '#' =>
+            if counts.headOption.contains(seen)
+            then 0L
+            else matches(states.tail, counts, seen + 1)
+          case '?' =>
+            matches('#' +: states.tail, counts, seen) +
+              matches('.' +: states.tail, counts, seen)
+          case c =>
+            require(false, s"invalid: $c"); ???
 
   /// reading & parsing
 
@@ -40,28 +42,27 @@ class Day12 extends munit.FunSuite:
 
   /// part 1
 
-  def part1(name: String): Int =
+  def part1(name: String): Long =
     getInput(name)
       .map: (states, counts) =>
-        matches(states, counts)
+        matches(states, counts, 0)
       .sum
   test("part 1 sample"):
-    assertEquals(part1("day12-sample.txt"), 21)
+    assertEquals(part1("day12-sample.txt"), 21L)
   test("part 1"):
-    assertEquals(part1("day12.txt"), 7361)
+    assertEquals(part1("day12.txt"), 7361L)
 
   /// part 2
 
-  def part2(name: String): Int =
+  def part2(name: String): Long =
     getInput(name)
       .map: (states, counts) =>
-        println((states, counts))
-        matches(Vector.fill(5)('?' +: states).flatten.tail, Vector.fill(5)(counts).flatten)
+        matches(Vector.fill(5)('?' +: states).flatten.tail, Vector.fill(5)(counts).flatten, 0)
       .sum
 
   test("part 2 sample"):
-    assertEquals(part2("day12-sample.txt"), 525152)
+    assertEquals(part2("day12-sample.txt"), 525152L)
   test("part 2"):
-    assertEquals(part2("day12.txt"), 0)
+    assertEquals(part2("day12.txt"), 83317216247365L)
 
 end Day12
