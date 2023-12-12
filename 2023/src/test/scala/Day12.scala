6254834f0e8bc42c25a0ab2b0c7399228132a536
Seth Tisue <seth@tisue.net>
2023-12-12 08:27:47 -0800

    day 12 part 1 not right yet

--- 2023/src/test/scala/Day12.scala
+++ 2023/src/test/scala/Day12.scala
@@ -1,6 +1,35 @@
+/*
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+*/
+
 class Day12 extends munit.FunSuite:
 
   /// core logic
+
+  def matches(states: Vector[Char], counts: Vector[Int]): Int =
+    if states.isEmpty
+    then
+      if counts.isEmpty
+      then 1
+      else 0
+    else states.head match
+      case '.' =>
+        matches(states.tail, counts)
+      case '#' =>
+        val firstCount = counts.head
+        if firstCount == 0
+        then 0
+        else matches(states.tail, (firstCount - 1) +: counts.tail)
+      case '?' =>
+        matches('#' +: states.tail, counts) +
+          matches('.' +: states.tail, counts)
+      case c =>
+        throw new RuntimeException(s"invalid input char: $c")
 
   /// reading & parsing
 
@@ -18,8 +47,12 @@ class Day12 extends munit.FunSuite:
   /// part 1
 
   def part1(name: String): Int =
-    getInput(name).size
-
+    getInput(name)
+      .tapEach(println)
+      .map: (states, counts) =>
+        matches(states, counts)
+      .tapEach(println)
+      .sum
   test("part 1 sample"):
     assertEquals(part1("day12-sample.txt"), 21)
 /*
