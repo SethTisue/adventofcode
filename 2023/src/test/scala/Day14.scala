dd8878499d2931db1bb07a74a361a4886c3c5aa7
Seth Tisue <seth@tisue.net>
2023-12-13 21:18:35 -0800

    day 14 part 1

--- 2023/src/test/scala/Day14.scala
+++ 2023/src/test/scala/Day14.scala
@@ -2,24 +2,56 @@ class Day14 extends munit.FunSuite:
 
   /// core logic
 
+  def rollOnce(s: String): String =
+    var s2 = s
+    for i <- s.indices.init
+    do
+      if s2(i) == 'O' && s2(i + 1) == '.'
+      then s2 = s2.updated(i, '.').updated(i + 1, 'O')
+    s2
+
+  def roll(s: String): String =
+    val result = rollOnce(s)
+    if s == result
+    then s
+    else roll(result)
+
+  def score(s: String): Int =
+    (for
+      (c, n) <- s.zipWithIndex
+      if c == 'O'
+    yield n + 1).sum
+
   /// reading & parsing
+
+  // each column is independent, so we `transpose` to get
+  // columns instead of rows. and `reverse` so the load
+  // will increase with increasing index
 
   def getInput(name: String): Vector[String] =
     io.Source.fromResource(name)
       .getLines
+      .map(_.toVector)
       .toVector
+      .transpose
+      .map(_.mkString.reverse)
 
   /// part 1
 
   def part1(name: String): Int =
-    getInput(name).size
+    getInput(name)
+      .tapEach(println)
+      .map(roll)
+      .tapEach(println)
+      .map(score)
+      .sum
 
   test("part 1 sample"):
     assertEquals(part1("day14-sample.txt"), 136)
-/*
   test("part 1"):
-    assertEquals(part1("day14.txt"), 0)
+    assertEquals(part1("day14.txt"), 105208)
 
+/*
   /// part 2
 
   def part2(name: String): Int =
