b2cd0eb9ce8fa7882430031aa0b22b07f54aedb1
Seth Tisue <seth@tisue.net>
2024-12-08 13:01:55 -0800

    2024 day 8 refactor

--- 2024/src/test/scala/Day08.scala
+++ 2024/src/test/scala/Day08.scala
@@ -2,8 +2,22 @@ class Day08 extends munit.FunSuite:
 
   /// core logic
 
-  case class Antenna(row: Int, column: Int, frequency: Char)
-  case class Board(size: Int, antennae: Set[Antenna])
+  type Position = (Int, Int)
+
+  case class Antenna(pos: Position, frequency: Char)
+
+  case class Board(size: Int, antennae: Set[Antenna]):
+    def inBounds(pos: Position) =
+      pos._1 >= 0 && pos._1 < size &&
+        pos._2 >= 0 && pos._2 < size
+    def pairs: Set[(Antenna, Antenna)] =
+      for a1 <- antennae
+          a2 <- antennae
+          if a1.frequency == a2.frequency && a1 != a2
+      yield (a1, a2)
+    def antinodes(a1: Antenna, a2: Antenna): LazyList[Position] =
+      LazyList.iterate((a2.pos._1, a2.pos._2)){case (r, c) =>
+        (r + a2.pos._1 - a1.pos._1, c + a2.pos._2 - a1.pos._2)}
 
   /// reading & parsing
 
@@ -11,28 +25,24 @@ class Day08 extends munit.FunSuite:
     val lines =
       io.Source.fromResource(name)
         .getLines
-        .toVector
+        .toIndexedSeq
     Board(lines.size,
       for row <- (0 until lines.size).toSet
           column <- 0 until lines.size
           freq = lines(row).charAt(column)
           if freq != '.'
-      yield Antenna(row, column, freq))
+      yield Antenna((row, column), freq))
 
   /// part 1
 
   def part1(name: String): Int =
     val board = getInput(name)
-    val antinodes =
-      for a1 <- board.antennae
-          a2 <- board.antennae
-          if a1.frequency == a2.frequency && a1 != a2
-          anti1 = (a1.row * 2 - a2.row, a1.column * 2 - a2.column)
-          anti2 = (a2.row * 2 - a1.row, a2.column * 2 - a1.column)
-      yield Set(anti1, anti2)
-    def inBounds(pos: (Int, Int)) =
-      pos._1 >= 0 && pos._1 < board.size && pos._2 >=0 && pos._2 < board.size
-    antinodes.flatten.filter(inBounds).size
+    val as =
+      for (a1, a2) <- board.pairs
+          anti1 = board.antinodes(a1, a2).drop(1).head
+          anti2 = board.antinodes(a2, a1).drop(1).head
+      yield Set(anti1, anti2).filter(board.inBounds)
+    as.flatten.size
 
   test("part 1 sample"):
     assertEquals(part1("day08-sample.txt"), 14)
@@ -43,20 +53,12 @@ class Day08 extends munit.FunSuite:
 
   def part2(name: String): Int =
     val board = getInput(name)
-    def inBounds(pos: (Int, Int)) =
-      pos._1 >= 0 && pos._1 < board.size && pos._2 >= 0 && pos._2 < board.size
-    val antinodes =
-      for a1 <- board.antennae
-          a2 <- board.antennae
-          if a1.frequency == a2.frequency && a1 != a2
-          antis1 = LazyList.iterate((a2.row, a2.column)){case (r, c) =>
-              (r + a2.row - a1.row, c + a2.column - a1.column)}
-            .takeWhile(inBounds).toSet
-          antis2 = LazyList.iterate((a1.row, a1.column)){case (r, c) =>
-              (r + a1.row - a2.row, c + a1.column - a2.column)}
-            .takeWhile(inBounds).toSet
-      yield (antis1 ++ antis2)
-    antinodes.flatten.filter(inBounds).size
+    val as =
+      for (a1, a2) <- board.pairs
+          antis1 = board.antinodes(a1, a2).takeWhile(board.inBounds)
+          antis2 = board.antinodes(a2, a1).takeWhile(board.inBounds)
+      yield (antis1.toSet ++ antis2.toSet)
+    as.flatten.size
 
   test("part 2 sample"):
     assertEquals(part2("day08-sample.txt"), 34)
